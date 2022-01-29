6a2b373d7b0804fc3e3f9df6ec34623a81a249a6
Lauriichan <laura.endress@syntaxphoenix.com>
2022-01-29 14:54:39 +0100

    Update githubUpdater.lua

--- githubUpdater.lua
+++ githubUpdater.lua
@@ -3,8 +3,9 @@ REPOSITORY = "Lauriichan/CC-Scripts"
 BRANCH = "master"
 GITHUB_URL = "https://raw.githubusercontent.com/" .. REPOSITORY .. "/" .. BRANCH .. "/"
 EXTENSION = ".lua"
+term.clear();
 if(#args < 1) then
-    print("ERROR: Please specify the file that you want to update")
+    term.error("Please specify the file that you want to update")
     return
 end
 local name = args[1]
@@ -14,11 +15,11 @@ end
 local url = GITHUB_URL .. args[1] .. EXTENSION
 local result = http.get(url)
 if(result.code == 404) then
-    print("ERROR: File " .. args[1] .. " doesn't exist!")
+    term.error("File " .. args[1] .. " doesn't exist!")
     return
 end
 if(result.code ~= 200) then
-    print("ERROR: Couldn't download file " .. args[1] .. "!")
+    term.error("Couldn't download file " .. args[1] .. "!")
     return
 end
 local content = result.readAll()
@@ -27,4 +28,4 @@ file.write(content)
 file.flush()
 file.close()
 result.close()
-print("Successfully downloaded file " .. args[1] .. " as " .. name .. "!")
+term.success("Successfully downloaded file " .. args[1] .. " as " .. name .. "!")
