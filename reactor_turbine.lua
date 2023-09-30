e86da25a2972bb526fd3422e050c59bbeea67720
Lauriichan <lauriichan@playuniverse.org>
2023-09-30 13:31:58 +0200

    Fix turbine

--- reactor_turbine.lua
+++ reactor_turbine.lua
@@ -1,9 +1,10 @@
 local turbine = peripheral.find("BiggerReactors_Turbine");
-local montior = peripheral.wrap("left");
+local monitor = peripheral.wrap("left");
 
 local battery = turbine.battery();
 
 monitor.setCursorBlink(false);
+monitor.clear();
 
 local capacity = battery.capacity();
 
@@ -12,28 +13,34 @@ local size = monitor.getSize();
 local width = size[0];
 local height = size[1];
 
-local centerY = (height / 2) - 1;
+local centerY = (height / 2) - 3;
 
 local ticks = 0;
 local stored = 0;
 while true do
+    sleep(0.5)
     stored = battery.stored();
-    monitor.clear();
+
     monitor.setCursor(0, centerY);
-    
+    monitor.clearLine();
     monitor.write("Capacity: " + capacity + " FE");
-    monitor.setCursor(0, centerY - 1);
+
+    monitor.setCursor(0, centerY + 1);
+    monitor.clearLine();
     monitor.write("Stored: " + stored +  " FE");
 
     if (stored == capacity) then
         ticks = ticks + 1;
-        if (ticks > 50) then
-            monitor.setCursor(0, centerY - 2);
-            monitor.write("Disabled: " + ticks);
-            redstone.setAnalogueOutput("bottom", 15);
+        if (ticks > 10) then
+            redstone.setAnalogueOutput("bottom", 0);
+            turbine.setActive(false);
         end
-    elseif (ticks > 50) then
+    elseif (ticks > 10) then
         ticks = 0;
-        redstone.setAnalogueOutput("bottom", 0);
+        redstone.setAnalogueOutput("bottom", 15);
+        turbine.setActive(true);
     end
+    monitor.setCursor(0, centerY + 2);
+    monitor.clearLine();
+    monitor.write("Disabled: " + ticks);
 end
